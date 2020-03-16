f578e104dd61c991088099f6e137cad73f48e276
Mochalov Oleksandrt <molych69@gmail.com>
2020-03-16 06:57:07 +0200

    redux-sandbox

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './counter'
+
+const App=()=>{
+    return <Counter />
+}
+
+export default App
+
+// counter={store.getState()}
+//         inc={inc}
+//         dec={dec}
+//         rnd={() => {
+//           const value = Math.floor(Math.random() * 10);
+//           rnd(value);
